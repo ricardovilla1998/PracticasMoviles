1cefec8c961f79bbf4ac82ffbd4e6985a05218b1
ricardovilla1998 <rialvillaos@ittepic.edu.mx>
2020-02-06 18:05:42 -0700

    Practica1_U1

---
+++ LADM_U1_Practica1_RicardoVilla/app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_ricardovilla/Main2Activity.kt
@@ -0,0 +1,12 @@
+package mx.edu.ittepic.ladm_u1_practica1_ricardovilla
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
---
+++ LADM_U1_Practica1_RicardoVilla/app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_ricardovilla/MainActivity.kt
@@ -0,0 +1,12 @@
+package mx.edu.ittepic.ladm_u1_practica1_ricardovilla
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
